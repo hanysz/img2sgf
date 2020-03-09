141f5c045df59e7bf943906306f08897f49880a7
hanysz <misc@hanysz.net>
2020-03-09 21:22:01 +1030

    refactor and add more output

--- img2sgf.py
+++ img2sgf.py
@@ -16,6 +16,7 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+from enum import Enum
 import sys, math
 
 input_file = sys.argv[1] # To do: sanity checking of command line arguments
@@ -27,6 +28,15 @@ maxblur = 2
 angle_delta = math.pi/180 # accept lines up to 2 degrees away from horizontal or vertical
 min_grid_spacing = 10
 show_steps = True
+#show_steps = False
+
+class Direction(Enum):
+  HORIZONTAL = 1
+  HORIZ = 1
+  H = 1
+  VERTICAL = 2
+  VERT = 2
+  V = 2
 
 input_image = cv.imread(input_file)
 grey_image = cv.cvtColor(input_image, cv.COLOR_BGR2GRAY)
@@ -73,45 +83,79 @@ if show_steps:
   plt.figure(3)
   plt.title("Circles removed")
   plt.imshow(circles_removed_image)
-hlines = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, threshold=threshold, \
-          min_theta = math.pi/2 - angle_delta, max_theta = math.pi/2 + angle_delta)
-if hlines is None:
-  sys.exit("Error: no horizontal lines found")
-vlines1 = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, threshold=threshold, \
-          min_theta = 0, max_theta = angle_delta)
-vlines2 = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, threshold=threshold, \
-          min_theta = math.pi - angle_delta, max_theta = math.pi)
-if vlines1 is None and vlines2 in None:
-  sys.exit("Error: no vertical lines found")
-if vlines2 is not None:
-  vlines2[:,0,0] = -vlines2[:,0,0]
-  vlines2[:,0,1] = vlines2[:,0,1] - math.pi
-  if vlines1 is not None:
-    vlines = np.vstack((vlines1, vlines2))
-  else:
-    vlines = vlines2
-else:
-  vlines = vlines1
 
-all_lines = np.vstack((hlines, vlines))
+
+
+def find_lines(threshold, direction):
+  if direction == Direction.H:
+    lines = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, \
+                          threshold=threshold, min_theta = math.pi/2 - angle_delta, \
+                          max_theta = math.pi/2 + angle_delta)
+  else:
+    vlines1 = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, \
+                            threshold=threshold, min_theta = 0, max_theta = angle_delta)
+    vlines2 = cv.HoughLines(circles_removed_image, rho=1, theta=math.pi/180.0, \
+                            threshold=threshold, min_theta = math.pi - angle_delta, \
+                            max_theta = math.pi)
+    if vlines2 is not None:
+      vlines2[:,0,0] = -vlines2[:,0,0]
+      vlines2[:,0,1] = vlines2[:,0,1] - math.pi
+      if vlines1 is not None:
+        lines = np.vstack((vlines1, vlines2))
+      else:
+        lines = vlines2
+    else:
+      lines = vlines1
+  return lines[:,0,0].reshape(-1,1) # clustering function prefers this shape
+
+hlines = find_lines(threshold, Direction.HORIZONTAL)
+vlines = find_lines(threshold, Direction.VERTICAL)
 plt.figure(4)
 plt.title("Lines found at threshold " + str(threshold))
-plt.scatter(all_lines[:,0,0], all_lines[:,0,1], marker=".")
+#plt.scatter(all_lines[:,0,0], all_lines[:,0,1], marker=".")
+if hlines is not None:
+  plt.scatter(hlines, len(hlines)*[0], marker=".")
+if vlines is not None:
+  plt.scatter(vlines, len(vlines)*[1], marker=".")
 
-hcentres = hlines[:,0,0].reshape(-1,1)
-vcentres = vlines[:,0,0].reshape(-1,1)
-cluster_model = AgglomerativeClustering(n_clusters=None, linkage = 'single',  \
+
+def find_clusters_fixed_threshold(threshold, direction):
+  lines = find_lines(threshold, direction)
+  cluster_model = AgglomerativeClustering(n_clusters=None, linkage = 'single',  \
                              distance_threshold=min_grid_spacing) 
-hclusters = cluster_model.fit(hcentres)
-print("Got " + str(hclusters.n_clusters_) + " horizontal lines")
+  return cluster_model.fit(lines)
+
+def get_cluster_centres(model, points):
+  n = model.n_clusters_
+  answer = np.zeros(n)
+  for i in range(n):
+    this_cluster = points[model.labels_ == i]
+    answer[i] = this_cluster.mean()
+  return answer
+
+hclusters = find_clusters_fixed_threshold(threshold, Direction.HORIZ)
+hcentres = get_cluster_centres(hclusters, hlines)
+vclusters = find_clusters_fixed_threshold(threshold, Direction.VERT)
+vcentres = get_cluster_centres(vclusters, vlines)
 colours = 10*['r.','g.','b.','c.','k.','y.','m.']
 
 plt.figure(5)
-plt.title("Lines coloured by cluster")
 for i in range(len(hlines)):
-   plt.plot(hlines[i,0,0], hlines[i,0,1], colours[hclusters.labels_[i]])
-vclusters = cluster_model.fit(vcentres)
-print("Got " + str(vclusters.n_clusters_) + " vertical lines")
+   plt.plot(hlines[i], 0, colours[hclusters.labels_[i]])
+plt.title("Got " + str(hclusters.n_clusters_) + " horizontal and " \
+          + str(vclusters.n_clusters_) + " vertical lines")
 for i in range(len(vlines)):
-   plt.plot(vlines[i,0,0], vlines[i,0,1], colours[vclusters.labels_[i]])
+   plt.plot(vlines[i], 1, colours[vclusters.labels_[i]])
+for i in hcentres:
+  plt.plot(i, 0, marker="x")
+for i in vcentres:
+  plt.plot(i, 1, marker="x")
+
+plt.figure(6)
+plt.title("Output")
+plt.imshow(input_image)
+for y in hcentres:
+  plt.plot((min(vcentres), max(vcentres)), (y,y), 'g')
+for x in vcentres:
+  plt.plot((x,x), (min(hcentres), max(hcentres)), 'g')
 plt.show()
